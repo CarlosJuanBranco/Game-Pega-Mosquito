d4345ce569122e8e5be8e7a8277a146e0042fff8
Juan Branco <contato.juanbranco@outlook.com>
2025-12-02 05:14:49 -0300

    Adding a game over screen

--- app_mosquito/jogo.js
+++ app_mosquito/jogo.js
@@ -1,6 +1,6 @@
-
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
@@ -11,22 +11,44 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo();
 
-var posicaoX = Math.floor(Math.random() * largura) - 90;
-var posicaoY = Math.floor(Math.random() * altura) - 90;
-
-posicaoX = posicaoX < 0 ? 0 : posicaoX;
-posicaoY = posicaoY < 0 ? 0 : posicaoY;
-
-console.log(posicaoX, posicaoY);
-
-// criar o elemento html
+    // criar o elemento html
 function posicaoRandomica() {
+
+    // remover o mosquito anterior (caso exista)
+    if (document.getElementById("mosquito")) {
+    document.getElementById("mosquito").remove();
+
+        //console.log("Elemento selecionado foi: v " + vidas);
+        if(vidas > 3) {
+
+            window.location.href = "fim_de_jogo.html";
+
+        } else {
+            document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png";
+
+            vidas++;
+        }
+
+}
+    // alterando a posição do mosquito na tela
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX, posicaoY);
+
     var mosquito = document.createElement("img");
     mosquito.src = "imagens/mosquito.png";
-    mosquito.className = tamanhoAleatorio();
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
     mosquito.style.left = posicaoX + "px";
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = "absolute";
+    mosquito.id = "mosquito";
+    mosquito.onclick = function() {
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
 }
@@ -41,5 +63,16 @@ function tamanhoAleatorio() {
             return "mosquito2";
         case 2:
             return "mosquito3";
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+
+    switch (classe) {
+        case 0:
+            return "ladoA";
+        case 1:
+            return "ladoB";
     }
 }
